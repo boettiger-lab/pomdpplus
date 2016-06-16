66eac84ae9830e506941b8fd9817c991a87d18c8
Milad Memarzadeh <memarzadeh.milad@gmail.com>
2016-06-15 22:43:06 -0700

    update

--- R/init_models.R
+++ R/init_models.R
@@ -10,7 +10,7 @@
 #' @return aa list of actions corresponding to alpha vectors for all candidate models; length = Num_Model
 #' @importFrom xml2 read_xml xml_find_all xml_contents xml_attr
 #' @importFrom purrr map_dbl
-#' @importFrom appl writepomdp pomdpsol read_policy
+#' @importFrom appl write_pomdpx pomdpsol read_policy
 init_models <- function(T,O,R,GAMMA,Num_model = length(T),initial) {
 
 Num_s = dim(T[[1]])[1]
