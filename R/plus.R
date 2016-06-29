39e511e5014b1fca27cde4a50ef9ec929b71fcd7
Milad Memarzadeh <memarzadeh.milad@gmail.com>
2016-06-29 14:06:40 -0700

    updating readme

--- R/plus.R
+++ R/plus.R
@@ -8,12 +8,13 @@
 #' @param n_sample number of sample to use for planning under model uncertainty; default = 5
 #' @param initial initial belief state
 #' @param P prior probability of the models ; default is flat prior
+#' @param precision accuracy of value function for generating models
 #' @return df data farme including results of forward simulations
 #' @return posterior data frame of Posterior distribution of each candidate model at each time
 #' @return av list of alpha vectors for all candidate models; length = Num_Model
 #' @return aa list of actions corresponding to alpha vectors for all candidate models; length = Num_Model
 #' @export
-plus <- function(input,t = 100, Num_sim = 100, n_true, n_sample = 5, initial, P = (array(1,dim = length(input[[1]]))/ length(input[[1]]))){
+plus <- function(input,t = 100, Num_sim = 100, n_true, n_sample = 5, initial, P = (array(1,dim = length(input[[1]]))/ length(input[[1]])), precision = 0.001){
 
 # extracting model parameters from input
 T = input[[1]]
@@ -29,7 +30,7 @@ N = length(T)            # Number of candidate models
 
 ######################################################################################
 # initializing models for each candidate
-out1 <- init_models(T,O,R,GAMMA,N,initial)
+out1 <- init_models(T,O,R,GAMMA,N,initial,precision)
 
 av = out1[[1]]
 aa = out1[[2]]
