682f671a7644b2abe453a6693321cb3677d79e7d
Carl Boettiger <cboettig@gmail.com>
2016-08-08 18:01:22 +0000

    account for prior belief over states

--- inst/notebook/pomdp_learning.R
+++ inst/notebook/pomdp_learning.R
@@ -1,36 +1,44 @@
 ## Assumes through-out that observation does not depend on action, and thus policy is determiend by observation alone and not observation + previous action
 
-pomdp_planning <- function(models, discount, model_prior, verbose = TRUE, mc.cores = 1L, ...){
-  Qs <- init_models(models, discount, verbose, mc.cores, ...)
-  compute_policy(Qs, model_prior)
+pomdp_planning <- function(models, discount, model_prior, states_prior = NULL, verbose = TRUE, mc.cores = 1L, ...){
+  alphas <- init_models(models = models, discount = discount, states_prior = states_prior, verbose = verbose, mc.cores = mc.cores, ...)
+  compute_policy(alphas, model_prior, states_prior, models)
 }
 
 
 
-init_models <- function(models, discount, verbose = TRUE, mc.cores = 1L, ...){
+init_models <- function(models, discount, states_prior = NULL,
+                          verbose = TRUE, mc.cores = 1L, ...){
 
-  parallel::mclapply(models, function(m){
+  n_states <- dim(models[[1]]$transition)[[1]]
+  n_actions <- dim(models[[1]]$transition)[[3]]
 
-    ## Assume uniform prior belief over states if not given for the model
-    if(is.null(m$initial)) initial <- rep(1, dim(m$observation)[[1]]) / dim(m$observation)[[1]]
+  if(is.null(states_prior))
+    states_prior <- vapply(1:length(models), function(i) rep(1, n_states) / n_states, numeric(n_states))
 
-    result <- run_pomdp(m$transition, m$observation, m$reward, discount, initial, verbose, ...)
 
-    V <- t(t(result$alpha) %*% m$observation[,,1])  ## Note the assumption that observation is indep of action
-    get_Q(V, result$alpha_action, dim(m$observation)[[3]])
+  parallel::mclapply(1:length(models), function(i){
+
+    belief <- states_prior[,i]
+    result <- run_pomdp(m$transition, m$observation, m$reward, discount, belief, verbose, ...)
+
+    regularize_alpha(result$alpha, result$alpha_action, n_actions)
+
 
   }, mc.cores = mc.cores)
 
 }
 
-compute_policy <- function(Qs, model_prior){
+compute_policy <- function(alphas, model_prior, states_prior){
 
+  belief <- states_prior
+
+  V <- t(t(alphas) %*% m$observation[,,1])  ## Note the assumption that observation is indep of action
   ## Compute optimal policy based on alpha vectors, V(b) = max_i \sum_x b(x) alpha_i(x)
   EV <- array(0, dim(Qs[[1]]))
   for(i in 1:length(Qs)){
     EV <- EV + as.matrix(Qs[[i]]) * model_prior[i]
   }
-
 
   value <- apply(EV, 1, max)
   policy <- apply(EV, 1, function(x) which.max(x))
@@ -42,7 +50,16 @@ compute_policy <- function(Qs, model_prior){
 }
 
 
-get_Q <- function(alpha, alpha_action, n_a){
+
+## POMDP solver returns a data.frame whose columns are the alpha vectors.
+## The action corresponding to each vector is given by alpha_action[i].
+##
+## Each alpha vector is of length n_states,  but there is not one vector for each
+## action -- some actions are not represented, others may be repeated (depends on #
+## of piecewise linear segments used to approximate value)
+##
+## So we create a new data.frame whose i'th column is the alpha vector for the i'th action
+regularize_alpha <- function(alpha, alpha_action, n_a){
   n_x <- dim(alpha)[[1]]
   unname(as.data.frame(
     lapply(1:n_a, function(i){
@@ -54,6 +71,11 @@ get_Q <- function(alpha, alpha_action, n_a){
     })
   ))
 }
+
+
+
+
+
 
 ###############
 
@@ -98,5 +120,14 @@ pomdp_learning <- function(x0, models, model_prior = NULL, Tmax = 20,
   list(df = df, posterior = posterior)
 }
 
+## Belief state & parameter state evolve through time! see equations 16 & 17
+
+bayes_update_pomdp <- function(qt, x_t, x_t1, a_t, transition, observation){
+
+  n_models <- length(transition)
+  P <- vapply(1:n_models, function(m) transition[[m]][x_t,x_t1,a_t], numeric(1))
+  qt * P / sum(qt * P)
+
+}
 
 
