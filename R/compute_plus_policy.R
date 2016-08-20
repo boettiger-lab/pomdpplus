6f3c770c8a385d9d5fd9a79e28f41726d24770b3
Carl Boettiger <cboettig@gmail.com>
2016-08-20 04:24:23 +0000

    draft compare method, also bigfix for compute_plus_policy

--- R/compute_plus_policy.R
+++ R/compute_plus_policy.R
@@ -14,15 +14,16 @@
 #' }
 compute_plus_policy <- function(alphas, models, model_prior = NULL, state_prior = NULL, a0 = 1){
 
-  n_states <- dim(alphas[[1]])[[1]]
-  n_actions <- dim(alphas[[1]])[[2]]
+  n_states <- dim(models[[1]]$observation)[[1]]
+  n_actions <- dim(models[[1]]$observation)[[3]]
   n_obs <- dim(models[[1]]$observation)[[2]]
   n_models <- length(models)
+  n_alpha <- length(alphas)
   if(is.null(state_prior)) state_prior <- rep(1, n_states) / n_states
   if(is.null(model_prior))  model_prior <- rep(1, n_models) / n_models
 
-  ## EV[i,j] is value of obs_state[i] when taking action j
-  EV <- array(0, dim = c(n_states, n_actions))
+
+  EV <- array(0, c(n_states, n_actions))
   for(j in 1:n_models){
     m <- models[[j]]
     ## belief[k,i] is belief system is in state k given observed state i
@@ -33,11 +34,30 @@ compute_plus_policy <- function(alphas, models, model_prior = NULL, state_prior 
                        else b / sum(b)
                      },
                      numeric(n_states))
-    EV <- EV + t(belief) %*% alphas[[j]]$vectors  * model_prior[j]
+    V <- t(belief) %*% alphas[[j]]$vectors  * model_prior[j]
+    EV <- EV + regularize_V(V, alphas[[j]]$action, n_actions)
   }
 
   value <- apply(EV, 1, max)
-  policy <- apply(EV, 1, function(x) alpha$action[which.max(x)])
+  policy <- apply(EV, 1, function(x) which.max(x))
 
   data.frame(policy, value, state = 1:n_obs)
 }
+
+
+regularize_V <- function(V, alpha_action, n_a){
+  n_x <- dim(V)[[1]]
+  vapply(1:n_a, function(i){
+    j <- which(alpha_action == i)
+    if(length(j) > 0){
+      if(length(j) > 1){
+        apply(V[, j], 1, max)
+      } else {
+        V[,j]
+      }
+    } else {
+      rep(0, n_x)
+    }
+  }, numeric(n_x))
+}
+
