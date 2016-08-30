09df28f8dd9a4f6cbc7b0b7726c8fb4e2a7a29a3
Carl Boettiger <cboettig@gmail.com>
2016-08-30 18:23:28 +0000

    fix logging when using sarsop_plus

--- R/sarsop_plus.R
+++ R/sarsop_plus.R
@@ -1,7 +1,7 @@
 #' sarsop_plus
 #'
 #' @inheritParams sim_plus
-#'
+#' @param log_data a data frame with information to be logged, one row for each model. Leave NULL if not logging
 #' @importFrom appl sarsop
 #' @return A list of alpha vector matrices
 #' @export
@@ -12,13 +12,20 @@
 #' unif <- compute_plus_policy(alphas, models, c(0.5, 0.5))
 #' }
 sarsop_plus <- function(models, discount, state_prior = NULL,
-                        verbose = TRUE, mc.cores = 1L, ...){
+                        verbose = TRUE, mc.cores = 1L, log_data = NULL, ...){
 
   n_states <- dim(models[[1]]$transition)[[1]]
   if(is.null(state_prior)) state_prior <- rep(1, n_states) / n_states
 
-  parallel::mclapply(models, function(m){
-    appl::sarsop(m$transition, m$observation, m$reward, discount, state_prior, verbose, ...)
+  parallel::mclapply(1:length(models), function(i){
+
+    m <- models[[i]]
+    if(!is.null(log_data) && dim(log_data)[1] > 1)
+      log_dat <- log_data[i,]
+    else
+      log_dat <- log_data
+
+    appl::sarsop(m$transition, m$observation, m$reward, discount, state_prior, verbose, log_data = log_dat, ...)
   }, mc.cores = mc.cores)
 
 }
--- tests/testthat/test-pomdp-learning.R
+++ tests/testthat/test-pomdp-learning.R
@@ -1,3 +1,6 @@
+library("appl")
+library("pomdpplus")
+## Problem definition
 states <- 0:20
 actions <- states
 obs <- states
@@ -5,15 +8,19 @@ sigma_g <- 0.1
 sigma_m <- sigma_g
 reward_fn <- function(x,h) pmin(x,h)
 discount <- 0.95
-K1 <- function(x, h, r = 1, K = 15){
+
+## Using two candidate models
+r <- 0.5
+K <- c(10,15)
+K1 <- function(x, h){
   s <- pmax(x - h, 0)
-  s * exp(r * (1 - s / K) )
+  s * exp(r * (1 - s / K[1]) )
 }
-K2 <- function(x, h, r = 1, K = 10){
+K2 <- function(x, h){
   s <- pmax(x - h, 0)
-  s * exp(r * (1 - s / K) )
+  s * exp(r * (1 - s / K[2]) )
 }
-models <- lapply(list(K1, K2), function(f) appl::fisheries_matrices(states, actions, obs, reward_fn, f, sigma_g, sigma_m))
+models <- lapply(list(K1,K2), function(f) appl::fisheries_matrices(states, actions, obs, reward_fn, f, sigma_g, sigma_m))
 
 
 alphas <- sarsop_plus(models, discount, precision = 1)
@@ -25,7 +32,29 @@ out <- sim_plus(models = models, discount = discount,
                 true_model = models[[2]],
                 alphas = alphas)
 
-testthat::testthat("plus prefers the true model after learning period", {
-  testthat::expect_gt(out$model_posterior[10,2] > out$model_posterior[10,1])
+testthat::test_that("plus prefers the true model after learning period", {
+  testthat::expect_gt(out$model_posterior[10,2], out$model_posterior[10,1])
 })
 
+
+
+
+## Check logging works
+log <- tempdir()
+log_data <- data.frame(model = "ricker", r = r, K = K, C = NA, sigma_g = sigma_g, sigma_m = sigma_m)
+
+alphas <- sarsop_plus(models, discount, precision = 1,
+                      log_dir = log, log_data = log_data)
+
+
+meta <- meta_from_log(parameters = data.frame(model = "ricker", r = r), log_dir = log)
+
+
+log_alphas <- alphas_from_log(meta, log_dir = log)
+log_models <- models_from_log(meta)
+
+
+testthat::expect_identical(alphas, log_alphas)
+testthat::expect_identical(models, log_models)
+
+log_fs <- f_from_log(meta)
